6974faba430ec427db63c60a60a6c3219a95534b
Eric Scuccimarra <skooch@gmail.com>
2018-09-27 11:49:34 +0200

    Updating chatbot.py with new code

--- chatbot/chatbot.py
+++ chatbot/chatbot.py
@@ -33,6 +33,81 @@ from tensorflow.python import debug as tf_debug
 from chatbot.textdata import TextData
 from chatbot.model import Model
 
+# since we aren't running this on the command line create an args object with default values
+class Args():
+    def __init__(self):
+        self.test = None
+        self.createDataset = False
+        self.playDataset = False
+        self.reset = False
+        self.verbose = True
+        self.debug = False
+        self.keepAll = False
+        self.modelTag = None
+        self.rootDir = None
+        self.watsonMode = False
+        self.autoEncode = False
+        self.device = "gpu"
+        self.seed = 0
+        
+        # dataset options
+        self.corpus = "opensubs"
+        self.datasetTag = ""
+        self.ratioDataset = 1.0
+        self.maxLength = 8
+        self.filterVocab = 20
+        self.skipLines = False
+        self.vocabularySize = 50000
+        
+        # network options
+        self.hiddenSize = 512
+        self.numLayers = 2
+        self.softmaxSamples = 0
+        self.initEmbeddings = False
+        self.embeddingSize = 56
+        self.embeddingSource = "GoogleNews-vectors-negative300.bin"
+        
+        # training options
+        self.numEpochs = 5
+        self.saveEvery = 2000
+        self.batchSize = 196
+        self.learningRate = 0.002
+        self.dropout = 0.9
+        
+# Copyright 2015 Conchylicultor. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+"""
+Main script. See README.md for more information
+
+Use python 3
+"""
+
+import configparser  # Saving the models parameters
+import datetime  # Chronometer
+import os  # Files management
+import tensorflow as tf
+import numpy as np
+import math
+
+from tqdm import tqdm  # Progress bar
+from tensorflow.python import debug as tf_debug
+
+from chatbot.textdata import TextData
+from chatbot.model import Model
+
 
 class Chatbot:
     """
@@ -66,7 +141,7 @@ class Chatbot:
         self.sess = None
 
         # Filename and directories constants
-        self.MODEL_DIR_BASE = 'save' + os.sep + 'model'
+        self.MODEL_DIR_BASE = 'save2' + os.sep + 'model'
         self.MODEL_NAME_BASE = 'model'
         self.MODEL_EXT = '.ckpt'
         self.CONFIG_FILENAME = 'params.ini'
@@ -77,64 +152,8 @@ class Chatbot:
 
     @staticmethod
     def parseArgs(args):
-        """
-        Parse the arguments from the given command line
-        Args:
-            args (list<str>): List of arguments to parse. If None, the default sys.argv will be parsed
-        """
-
-        parser = argparse.ArgumentParser()
-
-        # Global options
-        globalArgs = parser.add_argument_group('Global options')
-        globalArgs.add_argument('--test',
-                                nargs='?',
-                                choices=[Chatbot.TestMode.ALL, Chatbot.TestMode.INTERACTIVE, Chatbot.TestMode.DAEMON],
-                                const=Chatbot.TestMode.ALL, default=None,
-                                help='if present, launch the program try to answer all sentences from data/test/ with'
-                                     ' the defined model(s), in interactive mode, the user can wrote his own sentences,'
-                                     ' use daemon mode to integrate the chatbot in another program')
-        globalArgs.add_argument('--createDataset', action='store_true', help='if present, the program will only generate the dataset from the corpus (no training/testing)')
-        globalArgs.add_argument('--playDataset', type=int, nargs='?', const=10, default=None,  help='if set, the program  will randomly play some samples(can be use conjointly with createDataset if this is the only action you want to perform)')
-        globalArgs.add_argument('--reset', action='store_true', help='use this if you want to ignore the previous model present on the model directory (Warning: the model will be destroyed with all the folder content)')
-        globalArgs.add_argument('--verbose', action='store_true', help='When testing, will plot the outputs at the same time they are computed')
-        globalArgs.add_argument('--debug', action='store_true', help='run DeepQA with Tensorflow debug mode. Read TF documentation for more details on this.')
-        globalArgs.add_argument('--keepAll', action='store_true', help='If this option is set, all saved model will be kept (Warning: make sure you have enough free disk space or increase saveEvery)')  # TODO: Add an option to delimit the max size
-        globalArgs.add_argument('--modelTag', type=str, default=None, help='tag to differentiate which model to store/load')
-        globalArgs.add_argument('--rootDir', type=str, default=None, help='folder where to look for the models and data')
-        globalArgs.add_argument('--watsonMode', action='store_true', help='Inverse the questions and answer when training (the network try to guess the question)')
-        globalArgs.add_argument('--autoEncode', action='store_true', help='Randomly pick the question or the answer and use it both as input and output')
-        globalArgs.add_argument('--device', type=str, default=None, help='\'gpu\' or \'cpu\' (Warning: make sure you have enough free RAM), allow to choose on which hardware run the model')
-        globalArgs.add_argument('--seed', type=int, default=None, help='random seed for replication')
-
-        # Dataset options
-        datasetArgs = parser.add_argument_group('Dataset options')
-        datasetArgs.add_argument('--corpus', choices=TextData.corpusChoices(), default=TextData.corpusChoices()[0], help='corpus on which extract the dataset.')
-        datasetArgs.add_argument('--datasetTag', type=str, default='', help='add a tag to the dataset (file where to load the vocabulary and the precomputed samples, not the original corpus). Useful to manage multiple versions. Also used to define the file used for the lightweight format.')  # The samples are computed from the corpus if it does not exist already. There are saved in \'data/samples/\'
-        datasetArgs.add_argument('--ratioDataset', type=float, default=1.0, help='ratio of dataset used to avoid using the whole dataset')  # Not implemented, useless ?
-        datasetArgs.add_argument('--maxLength', type=int, default=10, help='maximum length of the sentence (for input and output), define number of maximum step of the RNN')
-        datasetArgs.add_argument('--filterVocab', type=int, default=1, help='remove rarelly used words (by default words used only once). 0 to keep all words.')
-        datasetArgs.add_argument('--skipLines', action='store_true', help='Generate training samples by only using even conversation lines as questions (and odd lines as answer). Useful to train the network on a particular person.')
-        datasetArgs.add_argument('--vocabularySize', type=int, default=40000, help='Limit the number of words in the vocabulary (0 for unlimited)')
-
-        # Network options (Warning: if modifying something here, also make the change on save/loadParams() )
-        nnArgs = parser.add_argument_group('Network options', 'architecture related option')
-        nnArgs.add_argument('--hiddenSize', type=int, default=512, help='number of hidden units in each RNN cell')
-        nnArgs.add_argument('--numLayers', type=int, default=2, help='number of rnn layers')
-        nnArgs.add_argument('--softmaxSamples', type=int, default=0, help='Number of samples in the sampled softmax loss function. A value of 0 deactivates sampled softmax')
-        nnArgs.add_argument('--initEmbeddings', action='store_true', help='if present, the program will initialize the embeddings with pre-trained word2vec vectors')
-        nnArgs.add_argument('--embeddingSize', type=int, default=64, help='embedding size of the word representation')
-        nnArgs.add_argument('--embeddingSource', type=str, default="GoogleNews-vectors-negative300.bin", help='embedding file to use for the word representation')
-
-        # Training options
-        trainingArgs = parser.add_argument_group('Training options')
-        trainingArgs.add_argument('--numEpochs', type=int, default=30, help='maximum number of epochs to run')
-        trainingArgs.add_argument('--saveEvery', type=int, default=2000, help='nb of mini-batch step before creating a model checkpoint')
-        trainingArgs.add_argument('--batchSize', type=int, default=256, help='mini-batch size')
-        trainingArgs.add_argument('--learningRate', type=float, default=0.002, help='Learning rate')
-        trainingArgs.add_argument('--dropout', type=float, default=0.9, help='Dropout rate (keep probabilities)')
-
-        return parser.parse_args(args)
+        args = Args()
+        return args
 
     def main(self, args=None):
         """
